86103e15929bad8a7667d3d42fefac41bbb5354a
CBRoyal <balascaleb2005@gmail.com>
2026-06-02 08:59:19 -0400

    Update index.js

---
+++ Royal Lion's Den_files/index.js
@@ -0,0 +1,5 @@
+function toggleMenu(){
+    const dropdownMenu = document.getElementById("royal_lions_den_website_phone_and_tablet_dropdown_navigation_menu");
+    dropdownMenu.style.display =
+      dropdownMenu.style.display === "block" ? "none" : "block";
+}
